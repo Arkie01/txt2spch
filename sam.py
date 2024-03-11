d7129d75dd8017e8bf5e7d777c6320fba134a8aa
Arkie01 <154900012+Arkie01@users.noreply.github.com>
2024-03-11 19:02:34 +0530

    sam.py

--- sam.py
+++ sam.py
@@ -1,16 +1,30 @@
+from flask import Flask, render_template, request
 import pyttsx3
 import PyPDF2
-from tkinter.filedialog import askopenfilename
 
-book = askopenfilename()
-pdfreader = PyPDF2.PdfReader(book)
-pages = len(pdfreader.pages)
+app = Flask(__name__)
 
-player = pyttsx3.init()
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-for num in range(0, pages):
-    page = pdfreader.pages[num]
-    text = page.extract_text()
-    player.say(text)
+@app.route('/upload', methods=['POST'])
+def upload():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
 
-player.runAndWait()
+        pdf_reader = PyPDF2.PdfReader(uploaded_file.filename)
+        pages = len(pdf_reader.pages)
+
+        player = pyttsx3.init()
+
+        text_content = []
+        for num in range(0, pages):
+            page = pdf_reader.pages[num]
+            text_content.append(page.extract_text())
+        
+        return render_template('result.html', text_content=text_content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
